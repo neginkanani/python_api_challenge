c15cc9d01a6e4d484daae4dab58b7df89a48d64a
neginkanani <118688095+neginkanani@users.noreply.github.com>
2023-01-22 22:32:54 -0500

    latest edits

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key ="544b1c3beaa3b05e47eee5b025d7893d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "544b1c3beaa3b05e47eee5b025d7893d"
